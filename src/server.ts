18c681f12f5528d503be61bd126565f56ce29466
danielazevedocb <daniel.santoscb@gmail.com>
2023-04-05 22:23:52 -0300

    Configurando swagger na aplicação e Criando documentação de Especialidade

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,15 @@ import express from "express";
 import { userRouter } from "./routes/user.routes";
 import { specialityRouter } from './routes/specialilty.routes';
 
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from "../swagger.json";
+
+
 const app = express();
 
 app.use(express.json())
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use(userRouter)
 app.use(specialityRouter)
